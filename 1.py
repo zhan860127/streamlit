424c6e51bd57b0f31f5ba5b65fd5fd44a7511a5c
tonyzhan <tonyzhan@vgame.com.tw>
2022-04-22 20:33:01 +0800

    add the example

---
+++ 1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('1.text顯示文本')
+st.markdown('**_really_cool** .')
+st.markdown(' ##### Streamlitis ')
+st.text('Thisissometext.')
+st.subheader('Thisisasubheader')
+st.write("st.write render 東西哦")
+st.warning('Thisisawarning')
